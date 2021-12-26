f5fe272363274e612f470829be7a761a53cebcf0
sumomo-99 <sumomo99.create@gmail.com>
2021-12-27 08:20:49 +0900

    rectangleプロジェクトを新規追加

---
+++ rectangle/README.md
@@ -0,0 +1,12 @@
+# Rectangle
+長方形の面積と周囲の長さを出力する。
+
+# Requirement
+* Rust
+
+# Usage
+縦3、横2の長方形の面積と周囲の長さを出力する。
+```bash
+cargo run -p rectangle -- 3 2
+// 6 10
+```
---
+++ rectangle/src/main.rs
@@ -0,0 +1,36 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "Rectangle")]
+
+struct Rectangle {
+    #[structopt(name = "length")]
+    a: u32,
+    #[structopt(name = "breadth")]
+    b: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.a * self.b
+    }
+
+    fn perimeter(&self) -> u32 {
+        2 * (self.a + self.b)
+    }
+}
+
+fn main() {
+    let rectangle = Rectangle::from_args();
+    println!("{} {}", rectangle.area(), rectangle.perimeter());
+}
+
+#[test]
+fn test() {
+    let t = Rectangle {
+        a: 3,
+        b: 5,
+    };
+    assert_eq!(t.area(), 15);
+    assert_eq!(t.perimeter(), 16);
+}
