0c26f4a09b4321bf9634adf01e1951a9c342520e
sumomo-99 <sumomo99.create@gmail.com>
2022-01-21 07:29:25 +0900

    体裁を修正

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -2,32 +2,38 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 #[structopt(name = "Rectangle")]
+///Calculate area and perimeter of rectangle.
 
 struct Rectangle {
     #[structopt(name = "length")]
-    a: u32,
+    ///Length of rectangle.
+    l: u32,
     #[structopt(name = "breadth")]
+    ///Breadth of rectangle.
     b: u32,
 }
 
 impl Rectangle {
     fn area(&self) -> u32 {
-        self.a * self.b
+        self.l * self.b
     }
 
     fn perimeter(&self) -> u32 {
-        2 * (self.a + self.b)
+        2 * (self.l + self.b)
     }
 }
 
 fn main() {
     let rectangle = Rectangle::from_args();
-    println!("{} {}", rectangle.area(), rectangle.perimeter());
+    println!("Length: {}", rectangle.l);
+    println!("Breadth: {}", rectangle.b);
+    println!("Area: {}", rectangle.area());
+    println!("Perimeter: {}", rectangle.perimeter());
 }
 
 #[test]
 fn test() {
-    let t = Rectangle { a: 3, b: 5 };
+    let t = Rectangle { l: 3, b: 5 };
     assert_eq!(t.area(), 15);
     assert_eq!(t.perimeter(), 16);
 }
