0cf241daf56c8fa2ac9a2817e4f7d5d405b4e16b
sumomo-99 <sumomo99.create@gmail.com>
2022-01-22 07:59:31 +0900

    x-powerのエラー処理を追加

--- x-power/src/main.rs
+++ x-power/src/main.rs
@@ -7,23 +7,30 @@ use structopt::StructOpt;
 struct Opt {
     #[structopt(name = "Integer")]
     ///Integer
-    a: u32,
-    #[structopt(name = "Power")]
-    ///Power
+    a: usize,
+    #[structopt(name = "Exponent")]
+    ///Exponent
     x: u32,
 }
 
-fn power(a: u32, x: u32) -> u32 {
-    a.pow(x)
+fn power(a: usize, x: u32) -> Result<usize, &'static str> {
+    match a.checked_pow(x) {
+        Some(result) => Ok(result),
+        None => Err("Overflow")
+    }
 }
 
 fn main() {
     let args = Opt::from_args();
-    println!("{}", power(args.a, args.x));
+    match power(args.a, args.x) {
+        Ok(result) => println!("{}の{}乗は{}です。", args.a, args.x, result),
+        Err(msg) => println!("計算できません。{}", msg),
+    }
 }
 
 #[test]
 fn test_power() {
-    assert_eq!(9, power(3, 2));
-    assert_eq!(27, power(3, 3));
+    assert_eq!(9, power(3, 2).unwrap());
+    assert_eq!(27, power(3, 3).unwrap());
+    assert!(power(10, 100).is_err());
 }
